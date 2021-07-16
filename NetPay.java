2a89231ced962f9c384c7643e685dfcda39949fb
featherykitty <87282811+featherykitty@users.noreply.github.com>
2021-07-16 18:20:50 -0400

    Update NetPay.java

--- NetPay.java
+++ NetPay.java
@@ -1,30 +1,4 @@
-/*
- * [CSCI 1301].java
- * Author:  [Pearl Takeda] 
- * Submission Date:  [01/24/2020]
- *
- * Purpose: The purpose of Lab 2 is to create a program that 
- *		calculates gross pay from hours worked per week, the amount of
- *		taxes (federal, state, social security, medicare) with given tax percentages
- * 		and ultimately net pay from deducting taxes from gross pay.
- * 
- * 
- * 
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither
- * received nor given inappropriate assistance. I have not copied
- * or modified code from any source other than the course webpage
- * or the course textbook. I recognize that any unauthorized
- * assistance or plagiarism will be handled in accordance with
- * the University of Georgia's Academic Honesty Policy and the
- * policies of this course. I recognize that my work is based
- * on an assignment created by the Department of Computer
- * Science at the University of Georgia. Any publishing 
- * or posting of source code for this assignment is strictly
- * prohibited unless you have written consent from the Department
- * of Computer Science at the University of Georgia.  
- */
+//01/24/2020
 import java.util.Scanner;
 public class NetPay {
 
